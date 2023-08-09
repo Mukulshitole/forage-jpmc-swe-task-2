a0f3b19f1e9cf4a26f8145b58939fc6af2794631
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2023-08-09 07:40:34 +0530

    server started

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -1,11 +1,8 @@
 export interface Order {
-  price: Number,
-  size: Number,
+  price: number,
+  size: number,
 }
-/**
- * The datafeed server returns an array of ServerRespond with 2 stocks.
- * We do not have to manipulate the ServerRespond for the purpose of this task.
- */
+
 export interface ServerRespond {
   stock: string,
   top_bid: Order,
@@ -14,24 +11,23 @@ export interface ServerRespond {
 }
 
 class DataStreamer {
-  // The url where datafeed server is listening
   static API_URL: string = 'http://localhost:8080/query?id=1';
 
-  /**
-   * Send request to the datafeed server and executes callback function on success
-   * @param callback callback function that takes JSON object as its argument
-   */
   static getData(callback: (data: ServerRespond[]) => void): void {
     const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
+    request.open('GET', DataStreamer.API_URL, true); // Use true for asynchronous
 
     request.onload = () => {
       if (request.status === 200) {
         callback(JSON.parse(request.responseText));
       } else {
-        alert ('Request failed');
+        console.error('Request failed');
       }
-    }
+    };
+
+    request.onerror = () => {
+      console.error('Request error');
+    };
 
     request.send();
   }
